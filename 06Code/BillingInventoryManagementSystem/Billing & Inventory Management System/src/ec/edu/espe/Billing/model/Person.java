ff18f06ec62470aed0769e535dd56709cb8692b9
Lomas Haro Christopher Josue <cjlomas@espe.edu.ec>
2026-04-30 17:45:53 -0500

    Add classes Person,Cashier,Supplier,Category

---
+++ 06Code/BillingInventoryManagementSystem/Billing & Inventory Management System/src/ec/edu/espe/Billing/model/Person.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.Billing.model;
+
+/**
+ *
+ * @author Christopher Lomas,<CodeBros,@ESPE>
+ */
+public class Person {
+    
+}
